c0ddc792fa90a29d75a2a9e62b62896923837e34
vibhor <vibhor27061995@gmail.com>
2023-09-13 16:42:37 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/mymaps/MapsAdapter.kt
@@ -0,0 +1,40 @@
+package com.example.mymaps
+
+import android.content.Context
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.mymaps.models.UserMap
+
+private const val TAG = "MapsAdapter"
+class MapsAdapter(val context: Context, val userMaps: List<UserMap>, val onClickListener: OnClickListener) : RecyclerView.Adapter<MapsAdapter.ViewHolder>() {
+
+    interface  OnClickListener{
+        fun onItemCLick(position: Int)
+    }
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(context).inflate(android.R.layout.simple_list_item_1,parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount() = userMaps.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val userMap = userMaps[position]
+        val textViewTitle = holder.itemView.findViewById<TextView>(android.R.id.text1)
+
+        holder.itemView.setOnClickListener {
+            Log.i(TAG, "Tapped on position $position")
+            onClickListener.onItemCLick(position)
+        }
+
+        textViewTitle.text = userMap.title
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+}
---
+++ app/src/main/java/com/example/mymaps/models/UserMap.kt
@@ -0,0 +1,5 @@
+package com.example.mymaps.models
+
+import java.io.Serializable
+
+data class UserMap(val title : String, val places: List<Place> ) : Serializable
